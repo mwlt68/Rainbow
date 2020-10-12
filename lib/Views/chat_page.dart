900690230e26ba46e7b2e0d992b2da2360a3e4e9
Mevlüt <mwltgr@gmail.com>
2020-10-13 00:07:50 +0300

    Chat UI update

--- lib/Views/chat_page.dart
+++ lib/Views/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:rainbow/Widgets/error_widgets.dart';
+
 class ChatPage extends StatefulWidget {
   @override
   _ChatPageState createState() => _ChatPageState();
@@ -8,28 +9,53 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     return Container(
-      child:  getMessages(),
+      child: getMessages(),
     );
   }
-  Widget getMessages()   {
+
+  Widget getMessages() {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('Chats').snapshots() ,
-      builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
-        if(snapshot.hasError){
-          return BasicErrorWidget(title: "Data could not load !",message: snapshot.error);
-        }
-        else if (snapshot.connectionState==ConnectionState.waiting)
-        {
-          return CircularProgressIndicator();
-        }
-        return ListView(
-            children: snapshot.data.docs.map((e) => ListTile(
-              title: Text(e['name']),
-              subtitle: Text(e['message']),
-            )).toList(),
+        stream: FirebaseFirestore.instance.collection('Chats').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return BasicErrorWidget(
+                title: "Data could not load !", message: snapshot.error);
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          return ListView(
+            children: snapshot.data.docs
+                .map((e) => ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage("https://picsum.photos/200"),
+                      ),
+                      title: Text(e['name']),
+                      subtitle: Text(e['message']),
+                      trailing: Column(
+                        children: [
+                          Text("19:34"),
+                          Container(
+                            margin: EdgeInsets.only(top:10),
+                            alignment:Alignment.center ,
+                            child: Text(
+                              "12",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            width: 25,
+                            height: 25,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Theme.of(context).accentColor),
+                          ),
+                        ],
+                      ),
+                    ))
+                .toList(),
           );
-    });
+        });
   }
 }
--- lib/Views/rainbow_main.dart
+++ lib/Views/rainbow_main.dart
@@ -12,14 +12,14 @@ class RainbowMain extends StatefulWidget {
 class _RainbowMainState extends State<RainbowMain>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
-  bool isVisibleMessageFAB=true;
+  bool isVisibleMessageFAB = true;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _tabController = new TabController(length: 4, vsync: this, initialIndex: 1);
     _tabController.addListener(() {
-      isVisibleMessageFAB=_tabController.index !=0;
+      isVisibleMessageFAB = _tabController.index != 0;
       setState(() {});
     });
   }
@@ -27,36 +27,59 @@ class _RainbowMainState extends State<RainbowMain>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Rainbow"),
-        bottom: TabBar(controller: _tabController, tabs: [
-          Tab(
-            icon: Icon(Icons.camera),
+        body: Container(
+          color: Theme.of(context).primaryColor,
+          child: SafeArea(
+              child: NestedScrollView(
+                
+                  headerSliverBuilder: (context, innerBoxScrool) {
+                    return [
+                      SliverAppBar(
+                        floating: true,
+                        title: Text("Rainbow"),
+                        actions: [
+                          IconButton(icon:Icon(Icons.search),onPressed: (){},),
+                          IconButton(icon:Icon(Icons.more_vert),onPressed: (){},),
+                        ],
+                      )
+                    ];
+                  },
+                  body: Column(
+                    children: [
+                      TabBar(controller: _tabController, tabs: [
+                        Tab(
+                          icon: Icon(Icons.camera),
+                        ),
+                        Tab(
+                          text: "Chat",
+                        ),
+                        Tab(
+                          text: "Status",
+                        ),
+                        Tab(
+                          text: "Calls",
+                        ),
+                      ]),
+                      Expanded(
+                          child: Container(
+                        color: Colors.white,
+                        child:
+                            TabBarView(controller: _tabController, children: [
+                          CameraPage(),
+                          ChatPage(),
+                          StatusPage(),
+                          CallsPage(),
+                        ]),
+                      ))
+                    ],
+                  ))),
+        ),
+        floatingActionButton: Visibility(
+          visible: isVisibleMessageFAB,
+          child: FloatingActionButton(
+            child: Icon(Icons.message),
+            onPressed: () {},
           ),
-          Tab(
-            text: "Chat",
-          ),
-          Tab(
-            text: "Status",
-          ),
-          Tab(
-            text: "Calls",
-          ),
-        ]),
-      ),
-      body: TabBarView(controller: _tabController, children: [
-        CameraPage(),
-        ChatPage(),
-        StatusPage(),
-        CallsPage(),
-      ]),
-      floatingActionButton:Visibility(
-        visible: isVisibleMessageFAB,
-        child: FloatingActionButton(
-        child: Icon(Icons.message),
-        onPressed: () {},
-      ),) 
-    );
+        ));
   }
 }
